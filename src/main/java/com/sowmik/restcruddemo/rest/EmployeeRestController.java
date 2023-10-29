d666ed9281e0dd633b449e72310fb1168857a681
sowmik-sec <sowmik.sec@gmail.com>
2023-10-29 12:59:45 +0600

    spring boot REST Get Single Employee

--- src/main/java/com/sowmik/restcruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/sowmik/restcruddemo/rest/EmployeeRestController.java
@@ -3,7 +3,9 @@ package com.sowmik.restcruddemo.rest;
 import com.sowmik.restcruddemo.dao.EmployeeDAO;
 import com.sowmik.restcruddemo.entity.Employee;
 import com.sowmik.restcruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
     private EmployeeService employeeService;
-    // quick and dirty: inject employee dao (use constructor injection
+    @Autowired
     public EmployeeRestController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
@@ -22,5 +24,15 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return theEmployee;
     }
 }
