a2a0e83d72d22622161fd54c8c7428472ac21df6
sowmik-sec <sowmik.sec@gmail.com>
2023-10-29 11:53:41 +0600

    create rest controller for getting all employees

---
+++ src/main/java/com/sowmik/restcruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,25 @@
+package com.sowmik.restcruddemo.rest;
+
+import com.sowmik.restcruddemo.dao.EmployeeDAO;
+import com.sowmik.restcruddemo.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeDAO employeeDAO;
+    // quick and dirty: inject employee dao (use constructor injection
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    // expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
