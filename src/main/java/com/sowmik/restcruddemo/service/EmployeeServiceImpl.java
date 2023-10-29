e31d1f5150c1f0606fe0d4fb33568ac707cd22e9
sowmik-sec <sowmik.sec@gmail.com>
2023-10-29 12:43:46 +0600

    spring boot service add, update, delete

--- src/main/java/com/sowmik/restcruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/sowmik/restcruddemo/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.sowmik.restcruddemo.dao.EmployeeDAO;
 import com.sowmik.restcruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -18,5 +19,22 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
